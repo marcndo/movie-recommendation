65575ee1fdcbcab6f53efc0d8342551e0d879fb5
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-01-15 01:57:42 +0100

    Add unit test for loading, preprocessing, and collaborating filtering

---
+++ src/tests/test_recommender.py
@@ -0,0 +1,27 @@
+import unittest
+from src import load_data
+from src import preprocess_data
+from src import collaborative_filtering 
+
+class TestRecommender(unittest.TestCase):
+
+    def test_load_data(self):
+        """Test loading data from csv file."""
+        ratings, movies = load_data()
+        self.assertFalse(ratings.empty, "Rating data can not be empty.")
+        self.assertFalse(movies.empty, 'Movies data can not be empty.')
+
+    def test_preprocess_data(self):
+        """Test for empty values in the data set."""
+        ratings, _ = load_data()
+        user_movie_interaction = preprocess_data(ratings)
+        self.assertEqual(user_movie_interaction.isnull().sum(), 0,'user_movie_interaction should contain no empty value')
+
+    def test_collaborative_filtering(self):
+        """Test the collaborative filtering logic."""
+        ratings, _ = load_data()
+        rmse = collaborative_filtering(ratings)
+        self.assertTrue(rmse > 0, "RMSE should be positive.")
+
+if __name__ == '__main__':
+    unittest.main()
