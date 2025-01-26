e4dbc43208d9758ad139802b246b8b67e12fb48a
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-01-26 01:10:39 +0100

    Optimize collaborative filtering

--- src/collaborative_filtering.py
+++ src/collaborative_filtering.py
@@ -1,24 +1,18 @@
-from surprise import Reader, Dataset, SVD, KNNBasic
+import numpy as np
+from surprise import Reader, Dataset, SVD
 from surprise.model_selection import train_test_split
-from surprise import accuracy
-from scipy.sparse import csr_matrix
+from surprise.model_selection import cross_validate
 
+algo = None
 def collaborative_filtering(ratings):
     """Build and evaluate a collaborative filtering"""
-    reader = Reader(rating_scale=(1,5))
-    data = Dataset.load_from_df(ratings[['userId', 'movieId', 'rating']], reader)
-    
-    #split dataset into train test splits
-    trainset, testset = train_test_split(data, test_size=0.2)
-
-    # Initialize the collaborative filtering algorithm (user-based)
-    #algo = KNNBasic(sim_options={'name': 'cosine', 'user_based': True})
-    algo = SVD()
-    algo.fit(trainset)
-
-    # Make predictions on the test set
-    predictions = algo.test(testset)
-    
-    # Calculate and return RMSE
-    rmse = accuracy.rmse(predictions)
-    return rmse
+    global algo
+    if algo is None:
+        reader = Reader(rating_scale=(1,5))
+        data = Dataset.load_from_df(ratings[['userId', 'movieId', 'rating']], reader)
+        np.random.seed(42)
+        trainset,_ = train_test_split(data, test_size=0.2)
+        algo = SVD()
+        algo.fit(trainset)
+        print('Model trained successfully!')
+    return algo
