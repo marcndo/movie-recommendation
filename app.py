1c5b639f6118f89ce01c442db1ba6faa16768e83
Marcel Ndowah <ndowahmarcel@gmail.com>
2025-01-26 04:02:25 +0100

    Fixed errors

--- app.py
+++ app.py
@@ -3,11 +3,21 @@ from src.recommender import collaborative_filtering, load_data, get_recommendati
 
 app = Flask(__name__)
 
+# Global variable to store the model
+ratings = None
+movies = None
+model = None
+
+@app.before_request
+def initialize_model():
+    global ratings, movies, model
+    ratings, movies = load_data()  # Load data once
+    model = collaborative_filtering(ratings)
+
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return "Welcome to the movie recommendation app!😊"
 
-@app.route('/recommend', methods=['GET'])
 @app.route('/recommend', methods=['GET'])
 def recommend():
     user_id = request.args.get('userId', type=int)
@@ -15,20 +25,22 @@ def recommend():
         return jsonify({'error': 'user id is required'}), 400
 
     try:
-        ratings, movies = load_data()
-        algo = collaborative_filtering(ratings)
-        recommendation = get_recommendation(user_id, algo, movies, ratings)
+        if not model:
+            return jsonify({"error": "Model not initialized"}), 500
+        print(f"User ID: {user_id}")
+        print(f"Model: {model}")
+        print(f"Movies: {movies.head()}")
+        print(f"Ratings: {ratings.head()}")
+        recommendation = get_recommendation(user_id,model,movies,ratings)
 
         if not recommendation:
-            return jsonify({'error': 'No recommendation found for this user'}), 404
+            return jsonify({'error': 'No recommendation found for this user. This could be due to insufficient data or ratings.'}), 404
         
         return jsonify({"recommendation": recommendation})
     
-    except ValueError as ve:
-        return jsonify({'error': str(ve)}), 404  # User ID not found
-    
     except Exception as e:
         print(f"Error during recommendation: {e}")
+        return jsonify({"error":"An error occurred during recommendation process"}), 500
 
 
 if __name__ == '__main__':
--- src/load_data.py
+++ src/load_data.py
@@ -1,19 +1,10 @@
 import pandas as pd
-
+ratings, movies = None, None
 
 def load_data():
-    """Load ratings and movies data set from csv files"""
-    ratings = pd.read_csv('data/rating.csv')
-    movies = pd.read_csv('data/movie.csv')
+    """Load ratings and movies data set from csv files only once"""
+    global ratings, movies
+    if ratings is None or movies is None:
+        ratings = pd.read_csv('data/rating.csv')
+        movies = pd.read_csv('data/movie.csv')
     return ratings, movies
-
-def explore_data(ratings, movies):
-    """Explore the data to check for missing values and 
-    descriptive statistics"""
-    print(ratings.head())
-    print(movies.head())
-    print(ratings.isnull().sum())
-    print(movies.isnull().sum())
-    print(ratings['rating'].describe())
-    ratings['rating'].value_counts().sort_index().plot(kind='bar')
-
